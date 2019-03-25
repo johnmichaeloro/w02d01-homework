4676fed757e2d60ae100701ccc45a80a0711681b
John Oro <johnmichaeloro@gmail.com>
2019-03-25 15:43:10 -0600

    object-ception

--- js/app.js
+++ js/app.js
@@ -62,7 +62,7 @@ console.log(john.hobby);
 **/
 
 //Crazy Object!
-
+/**
 const crazyObject = {
   taco: [
     {
@@ -99,3 +99,25 @@ console.log(crazyObject.larry.quotes[0]);
 console.log(crazyObject.larry.characters[2].favourtieHobby);
 console.log(crazyObject.larry.nicknames[1]);
 console.log(crazyObject.larry.characters[1]);
+**/
+
+//Object-ception
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo);
