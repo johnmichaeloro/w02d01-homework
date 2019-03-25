dc54998a5978f3f9a0ee711922ef9988cb893a7b
John Oro <johnmichaeloro@gmail.com>
2019-03-25 15:40:35 -0600

    crazyObject

--- js/app.js
+++ js/app.js
@@ -49,6 +49,7 @@ const class = [{name: "Gordon", location: "Boston"} {name: "Grundlina", location
 **/
 
 //Take It Easy
+/**
 const rainbow = ["red", "orange", "yellow", "green", "blue", "purple"];
 console.log(rainbow[4]);
 const john = {
@@ -58,3 +59,43 @@ const john = {
   favDataType: "Boolean"
 }
 console.log(john.hobby);
+**/
+
+//Crazy Object!
+
+const crazyObject = {
+  taco: [
+    {
+      meat: 'steak',
+      cheese: ['panela', 'queso', 'chihuahua']
+    },
+    {
+      meat: 'chicken',
+      salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+    },
+  ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [
+      {
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favourtieHobby: "Swearing at Larry and Jeff"
+      },
+    ]
+  }
+}
+console.log(crazyObject.taco[1].salsa[5]);
+console.log(crazyObject.larry.quotes[0]);
+console.log(crazyObject.larry.characters[2].favourtieHobby);
+console.log(crazyObject.larry.nicknames[1]);
+console.log(crazyObject.larry.characters[1]);
