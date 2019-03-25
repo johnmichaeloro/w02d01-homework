ee0b8904e20bfe99cc52adc573066c7f80e28bb8
John Oro <johnmichaeloro@gmail.com>
2019-03-25 15:31:13 -0600

    data structures

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 //When these datatypes are combined, we get a data structure, for example, an array that contains objects that each contain booleans.
 //Suppose you are tasked with creating some software, and it is up to you to determine which datatypes and what data structure to use. For each of the following, write which data types you would use to represent the data, and then give a small example of the data structure:
 //1. I would use a Boolean, in which on would be true and off false.
-
+/**
 const lightSwitch = {
   status: true,
   switch() {
@@ -46,3 +46,15 @@ const class = [{name: "Gordon", location: "Boston"} {name: "Grundlina", location
  //5. Same as above, just with a new property for favorite shows. It contains an array.
 
  const class = [{name: "Gordon", location: "Boston", favoriteTV: ["Cheers", "Desperate Housewives: the New Wives on the Block"]} {name: "Grundlina", location: "Pantston", favoriteTV: ["Cheers", "Desperate Housewives: the New Wives on the Block"]}];
+**/
+
+//Take It Easy
+const rainbow = ["red", "orange", "yellow", "green", "blue", "purple"];
+console.log(rainbow[4]);
+const john = {
+  favFood: "chicken",
+  hobby: "reading scary books about ghosts and frogmen",
+  town: "Denver",
+  favDataType: "Boolean"
+}
+console.log(john.hobby);
